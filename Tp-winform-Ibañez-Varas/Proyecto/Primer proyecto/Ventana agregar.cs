fac5e2b6bf2a6e9b8a2e1ff078230dbe7c68b0f1
Alan Ibañez <Alaanoriel@gmail.com>
2022-09-18 22:54:23 -0300

    Cambios

--- Tp-winform-Ibañez-Varas/Proyecto/Primer proyecto/Ventana agregar.cs
+++ Tp-winform-Ibañez-Varas/Proyecto/Primer proyecto/Ventana agregar.cs
@@ -34,13 +34,13 @@ namespace Primer_proyecto
             {
                 Producto.Codigo = Txtbox_CodigoArticulo.Text;
                 Producto.Nombre = Txtbox_Nombre.Text;
-                //Producto.Precio = Decimal.Round(Convert.ToDecimal(Txtbox_Precio),2);
+                Producto.Precio = Convert.ToDecimal(Txtbox_Precio.Text);
                 Producto.Descripcion = Txtbox_Descripcion.Text;
                 Producto.Marca = (Marca)ComboBox_Marca.SelectedItem;
                 Producto.Tipo = (Categorias)ComboBox_Categoria.SelectedItem;
                 Dato.Agregar(Producto);
                 MessageBox.Show("Agregado con exito");
-                Close();
+                
 
             }
 
